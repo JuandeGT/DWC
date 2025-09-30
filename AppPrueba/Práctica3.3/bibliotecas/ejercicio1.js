6a877311abf7ae9577d42a484dfa6b700eee63b2
Juande GT <juandegomez2005@gmail.com>
2025-09-30 20:29:12 +0200

    Práctica3.3 terminada

--- AppPrueba/Práctica3.3/bibliotecas/ejercicio1.js
+++ AppPrueba/Práctica3.3/bibliotecas/ejercicio1.js
@@ -1,15 +1,19 @@
-"use strict";
+'use strict';
 
 const curso = (nombre, anyo, alumnado, descripcion) => {
-	if (typeof descripcion == "undefined") {
+	if (typeof descripcion == 'undefined') {
 		// Comprobamos si no hay descripción para asignarle un valor por defecto
-		descripcion = "Descripcion del curso vacía";
+		descripcion = 'Descripcion del curso vacía';
 	}
 	return {
 		nombre: nombre,
 		anyo: anyo,
 		alumnado: [alumnado],
 		descripcion: descripcion,
+		//Método del ejercicio 4
+		matricular: function (discente) {
+			this.alumnado = [...this.alumnado, discente];
+		},
 	};
 };
 
--- AppPrueba/Práctica3.3/bibliotecas/ejercicio3.js
+++ AppPrueba/Práctica3.3/bibliotecas/ejercicio3.js
@@ -1,29 +1,32 @@
-"use strict";
+'use strict';
 
 const discente = (id, nombre, apellidos, aficiones, notas) => {
 	return {
 		id: id,
 		nombre: nombre,
 		apellidos: apellidos,
-		aficiones: [aficiones],
+		aficiones: aficiones,
 		notas: {
 			primeraNota: notas.primeraNota,
 			segundaNota: notas.segundaNota,
 			terceraNota: notas.terceraNota,
 		},
 		calcularMedia: function () {
-			let media = 0;
-			for (let i = 0; i < this.notas.length; i++) {
-				media += this.notas[i];
-			}
-			return media / notas.length;
+			return (this.notas.primeraNota + this.notas.segundaNota + this.notas.terceraNota) / 3;
 		},
 		imprimirAficiones: function () {
-			let imprimir = "Aficiones: ";
-			this.aficiones.forEach((visualViewport, InputDeviceInfo, array) => {
-				imprimir += `${dato}, `;
+			let imprimir = 'Aficiones: ';
+			this.aficiones.forEach((dato) => {
+				imprimir += `${dato} `;
 			});
 			return imprimir;
+		},
+		imprimirInforme: function () {
+			return `ID: ${this.id}\nNombre: ${this.nombre}\nApellidos: ${
+				this.apellidos
+			}\n${this.imprimirAficiones()}\nNotas:\nNota 1.ª Evaluación: ${this.notas.primeraNota}\nNota 2.ª Evaluación: ${
+				this.notas.segundaNota
+			}\nNota 3.ª Evaluación: ${this.notas.terceraNota}\n`;
 		},
 	};
 };
