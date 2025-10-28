21c26715c20b777c648133841a7d3f1ff59ed17c
Juande GT <juandegomez2005@gmail.com>
2025-10-28 14:05:17 +0100

    Práctica 4.07 continuada

--- AppPruebaReact/src/components/Practica4.07/pages/Interpretes.jsx
+++ AppPruebaReact/src/components/Practica4.07/pages/Interpretes.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
+import Peliculas from '../peliculas.json';
 
-const Interpretes = () => {
-	return <div>Interpretes</div>;
+const Interpretes = ({ id }) => {
+	const pelicula = Peliculas.peliculas.filter((peli) => {
+		p.id == id;
+	});
+	return (
+		<div className="actores">
+			{pelicula.actores.map((actor) => {
+				<div>
+					<h3>{actor.nombre}</h3>
+					<p>{actor.fechaNacimiento}</p>
+					<p>{actor.biografia}</p>
+					<img src={actor.imagen} alt="Imagen" />
+				</div>;
+			})}
+		</div>
+	);
 };
 
 export default Interpretes;
--- AppPruebaReact/src/components/Practica4.07/pages/Peliculas.jsx
+++ AppPruebaReact/src/components/Practica4.07/pages/Peliculas.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React from 'react';
+import Peliculas from '../peliculas.json';
+import Interpretes from './Interpretes.jsx';
 
 const Peliculas = () => {
-	return <div>Peliculas</div>;
+	return (
+		<div className="peliculas">
+			{Peliculas.peliculas.map((peli) => {
+				<div>
+					<h2>{peli.nombre}</h2>
+					<p>Director: {peli.director}</p>
+					<p>Clasificación: {peli.clasificacion}</p>
+					<p>Recaudación: {peli.recaudacion}</p>
+					<img src={peli.cartelera} alt="Cartelera" />
+					<p>Nota: {peli.nota}</p>
+					<p>Resumen: {peli.resumen}</p>
+					<h3>Actores:</h3>
+					<div>{<Interpretes id={peli.id} />}</div>
+				</div>;
+			})}
+		</div>
+	);
 };
 
 export default Peliculas;
