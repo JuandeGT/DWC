8fb00d04faf355bc8eae0b8ecf52f4a30de0724e
Juande <juaandegomez2005@gmail.com>
2025-10-29 16:01:54 +0100

    4.07 corregido

--- AppPruebaReact/src/components/Practica4.07/pages/Galeria.jsx
+++ AppPruebaReact/src/components/Practica4.07/pages/Galeria.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import SubMenu from '../estructura/SubMenu';
-import './Galeria.css';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import SubMenu from "../estructura/SubMenu";
 
 const Galeria = () => {
 	return (
