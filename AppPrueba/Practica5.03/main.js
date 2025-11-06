6d4d9228afdfbb7f1bf3fcb5ba2dd0c3aa8cf919
Juande GT <juandegomez2005@gmail.com>
2025-11-06 23:01:28 +0100

    5.03 terminada

--- AppPrueba/Practica5.03/main.js
+++ AppPrueba/Practica5.03/main.js
@@ -1,18 +1,15 @@
-"use strict";
+'use strict';
 
-import {
-	crearLienzo,
-	pintarLienzo,
-	reiniciarLienzo,
-} from "./bibliotecas/ejercicio1.js";
+import { crearLienzo, pintarLienzo, reiniciarLienzo } from './bibliotecas/ejercicio1.js';
 
 crearLienzo();
 
+const texto = document.getElementById('texto');
 let color;
 let colorSeleccionado = false;
-const colores = document.getElementById("colores");
-colores.addEventListener("click", (event) => {
-	if (event.target.parentNode.classList.value === "seleccionar") {
+const colores = document.getElementById('colores');
+colores.addEventListener('click', (event) => {
+	if (event.target.parentNode.classList.value === 'seleccionar') {
 		colorSeleccionado = true;
 	} else {
 		colorSeleccionado = false;
@@ -20,35 +17,43 @@ colores.addEventListener("click", (event) => {
 		const listaColores = colores.children;
 		for (let i = 0; i < listaColores.length; i++) {
 			if (listaColores[i].classList.value === color) {
-				listaColores[i].classList.add("seleccionado");
+				listaColores[i].classList.add('seleccionado');
 			} else {
-				listaColores[i].classList.remove("seleccionado");
+				listaColores[i].classList.remove('seleccionado');
 			}
+		}
+		if (color !== 'seccion') {
+			texto.innerHTML = `Color seleccionado: ${color}`;
+			texto.style.color = color;
 		}
 	}
 });
 
-const lienzo = document.getElementById("tabla");
+const lienzo = document.getElementById('tabla');
 let pintar = false;
-lienzo.addEventListener("mousedown", (event) => {
+lienzo.addEventListener('mousedown', (event) => {
 	pintar = true;
 	if (colorSeleccionado) {
-		color = document.getElementsByClassName("seleccionar")[0].children[0].value;
+		color = document.getElementsByClassName('seleccionar')[0].children[0].value;
+	}
+	if (color !== 'seccion') {
+		texto.innerHTML = `Color seleccionado: ${color}`;
+		texto.style.color = color;
 	}
 	pintarLienzo(event, color);
 });
 
-lienzo.addEventListener("mouseover", (event) => {
+lienzo.addEventListener('mouseover', (event) => {
 	if (pintar) {
 		pintarLienzo(event, color);
 	}
 });
 
 // El mouseup se hace el todo el documento en vez del lienzo para evitar que siga pintando en casos específicos
-document.addEventListener("mouseup", () => {
+document.addEventListener('mouseup', () => {
 	pintar = false;
 });
 
-document.getElementsByTagName("button")[0].addEventListener("click", () => {
+document.getElementsByTagName('button')[0].addEventListener('click', () => {
 	reiniciarLienzo();
 });
