7e93e280dc7b4da65930a299093d3d9133649a1a
Juande GT <juandegomez2005@gmail.com>
2025-11-10 12:25:19 +0100

    5.04 terminada, falta extra

--- AppPrueba/Practica5.04/bibliotecas/ejercicio1.js
+++ AppPrueba/Practica5.04/bibliotecas/ejercicio1.js
@@ -1,14 +1,16 @@
-"use strict";
+'use strict';
 
 const crearPiezas = (imagenes) => {
-	const piezas = document.getElementById("piezas");
-	for (let i = 0; i < imagenes.length; i++) {
-		piezas.insertAdjacentHTML(
-			"beforeend",
-			`<img id="${i + 1}" class="arrastable"/>`
-		);
-		document.getElementById(`${i + 1}`).src = imagenes[i];
+	const imagenesMezcladas = [...imagenes].sort(() => Math.random() - 0.5);
+	const piezas = document.getElementById('piezas');
+	for (let i = 0; i < imagenesMezcladas.length; i++) {
+		piezas.insertAdjacentHTML('beforeend', `<img id="${i + 1}" class="arrastable"/>`);
+		document.getElementById(`${i + 1}`).src = imagenesMezcladas[i];
 	}
 };
 
-export { crearPiezas };
+const crearBoton = () => {
+	document.getElementById('boton').insertAdjacentHTML('beforeend', '<button>Reiniciar</button>');
+};
+
+export { crearPiezas, crearBoton };
--- AppPrueba/Practica5.04/main.js
+++ AppPrueba/Practica5.04/main.js
@@ -1,69 +1,77 @@
-"use strict";
+'use strict';
 
-import { crearPiezas } from "./bibliotecas/ejercicio1.js";
+import { crearPiezas, crearBoton } from './bibliotecas/ejercicio1.js';
 
 window.onload = () => {
 	//Hacer temporizador
 	const imagenes = [
-		"./images/1.png",
-		"./images/2.png",
-		"./images/3.png",
-		"./images/4.png",
-		"./images/5.png",
-		"./images/6.png",
-		"./images/7.png",
-		"./images/8.png",
-		"./images/9.png",
+		'./images/1.png',
+		'./images/2.png',
+		'./images/3.png',
+		'./images/4.png',
+		'./images/5.png',
+		'./images/6.png',
+		'./images/7.png',
+		'./images/8.png',
+		'./images/9.png',
 	];
 
 	crearPiezas(imagenes);
+	crearBoton();
 
-	const arrastables = document.getElementsByClassName("arrastable");
+	const arrastables = document.getElementsByClassName('arrastable');
 	for (let i = 0; i < arrastables; i++) {
-		arrastables[i].setAttribute("draggable", true);
+		arrastables[i].setAttribute('draggable', true);
 	}
 
-	const piezas = document.getElementById("piezas");
+	const piezas = document.getElementById('piezas');
 	piezas.addEventListener(
-		"dragstart",
+		'dragstart',
 		(evento) => {
-			evento.dataTransfer.setData("id", evento.target.id);
+			evento.dataTransfer.setData('id', evento.target.id);
 		},
 		false
 	);
 
-	piezas.addEventListener("dragover", (evento) => {
+	piezas.addEventListener('dragover', (evento) => {
 		evento.preventDefault();
 	});
 
-	piezas.addEventListener("drop", (evento) => {
+	piezas.addEventListener('drop', (evento) => {
 		evento.preventDefault();
-		if (evento.target.classList.contains("soltable")) {
-			evento.target.appendChild(
-				document.getElementById(evento.dataTransfer.getData("id"))
-			);
+		if (evento.target.classList.contains('soltable') || evento.target.id === 'piezas') {
+			evento.target.appendChild(document.getElementById(evento.dataTransfer.getData('id')));
 		}
 	});
 
-	const panel = document.getElementById("panel");
+	const panel = document.getElementById('panel');
 	panel.addEventListener(
-		"dragstart",
+		'dragstart',
 		(evento) => {
-			evento.dataTransfer.setData("id", evento.target.id);
+			evento.dataTransfer.setData('id', evento.target.id);
 		},
 		false
 	);
 
-	panel.addEventListener("dragover", (evento) => {
+	panel.addEventListener('dragover', (evento) => {
 		evento.preventDefault();
 	});
 
-	panel.addEventListener("drop", (evento) => {
+	panel.addEventListener('drop', (evento) => {
 		evento.preventDefault();
-		if (evento.target.classList.contains("soltable")) {
-			evento.target.appendChild(
-				document.getElementById(evento.dataTransfer.getData("id"))
-			);
+		if (evento.target.classList.contains('soltable')) {
+			evento.target.appendChild(document.getElementById(evento.dataTransfer.getData('id')));
+		}
+	});
+
+	document.getElementsByTagName('button')[0].addEventListener('click', () => {
+		// Vaciamos el div con las imágenes para volver a crearlo.
+		piezas.innerHTML = '';
+		crearPiezas(imagenes);
+		// Vaciamos el panel para que no tenga imágenes
+		let panelImg = panel.children;
+		for (let i = 0; i < panelImg.length; i++) {
+			panelImg[i].innerHTML = '';
 		}
 	});
 }; //Fin del window onload
