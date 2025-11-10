f931c5ce425b8c36e551e8623654f074a94485bd
Juande GT <juandegomez2005@gmail.com>
2025-11-10 22:51:56 +0100

    5.04 terminada

--- AppPrueba/Practica5.04/bibliotecas/ejercicio1.js
+++ AppPrueba/Practica5.04/bibliotecas/ejercicio1.js
@@ -1,25 +1,31 @@
-"use strict";
+'use strict';
 
-const crearPiezas = (imagenes) => {
+const crearPiezas = (imagenes, nivel) => {
 	const imagenesMezcladas = [...imagenes].sort(() => Math.random() - 0.5);
-	const piezas = document.getElementById("piezas");
+	const piezas = document.getElementById('piezas');
 	for (let i = 0; i < imagenesMezcladas.length; i++) {
-		piezas.insertAdjacentHTML(
-			"beforeend",
-			`<img id="${imagenesMezcladas[i][15]}img" class="arrastable"/>`
-		);
-		document.getElementById(`${imagenesMezcladas[i][15]}img`).src =
-			imagenesMezcladas[i];
+		piezas.insertAdjacentHTML('beforeend', `<img id="${imagenesMezcladas[i][15]}img${nivel}" class="arrastable"/>`);
+		document.getElementById(`${imagenesMezcladas[i][15]}img${nivel}`).src = imagenesMezcladas[i];
 	}
 };
 
-const crearBoton = () => {
-	document
-		.getElementById("boton")
-		.insertAdjacentHTML("beforeend", "<button>Reiniciar</button>");
+const crearPiezasMedio = (piezas, panel, imgMedio) => {
+	piezas.innerHTML = '';
+	let panelImg = panel.children;
+	for (let i = 0; i < panelImg.length; i++) {
+		panelImg[i].innerHTML = '';
+	}
+	if (document.getElementById('resuelto')) {
+		document.getElementById('resuelto').remove();
+	}
+	crearPiezas(imgMedio, 'M');
 };
 
-const comprobarImg = (soltables) => {
+const crearBoton = () => {
+	document.getElementById('boton').insertAdjacentHTML('beforeend', '<button>Reiniciar</button>');
+};
+
+const comprobarImg = (soltables, veces) => {
 	let numAciertos = 0;
 	for (let i = 0; i < soltables.length; i++) {
 		if (soltables[i].firstChild) {
@@ -28,19 +34,29 @@ const comprobarImg = (soltables) => {
 			}
 		}
 	}
-	return numAciertos === 9 ? true : false;
+	if (numAciertos === 9) {
+		veces++;
+		return veces;
+	}
+	return veces;
 };
 
-const crearPiezasMedio = (piezas, panel, imgMedio) => {
-	piezas.innerHTML = "";
-	let panelImg = panel.children;
-	for (let i = 0; i < panelImg.length; i++) {
-		panelImg[i].innerHTML = "";
-	}
-	if (document.getElementById("resuelto")) {
-		document.getElementById("resuelto").remove();
-	}
-	crearPiezas(imgMedio);
+const cuentaAtras = (sitio, funcion) => {
+	let tiempo = 3;
+	sitio.insertAdjacentHTML('afterbegin', '<h2 id="cuentaAtras"></h2>');
+
+	const cuentaAtras = document.getElementById('cuentaAtras');
+	cuentaAtras.innerHTML = `Siguiente nivel ${tiempo}...`;
+
+	const intervalo = setInterval(() => {
+		tiempo--;
+		cuentaAtras.innerHTML = `Siguiente nivel ${tiempo}...`;
+
+		if (tiempo <= 0) {
+			clearInterval(intervalo);
+			funcion();
+		}
+	}, 1000);
 };
 
-export { crearPiezas, crearBoton, comprobarImg, crearPiezasMedio };
+export { crearPiezas, crearBoton, comprobarImg, crearPiezasMedio, cuentaAtras };
